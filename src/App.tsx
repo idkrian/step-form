536a369e8e8d0abab8a332cfc8cdce7b2e1592fe
Rian <riansouza0@gmail.com>
2023-12-19 23:19:09 -0300

    feat: project creation

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-white">
+      <div>
+        <h1>Form</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      lightYellow: "#f3f3e3",
+      salmon: "#fc6c4c",
+      white: "#ffffff",
+      black: "#19232b",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
